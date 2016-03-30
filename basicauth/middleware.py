23b08fe088107786a07458a161ba5787cbcfdb94
Raphaël Barrois <raphael.barrois@polytechnique.org>
2016-03-30 16:37:53 +0200

    Add a middleware-based authentication system.

---
+++ basicauth/middleware.py
@@ -0,0 +1,11 @@
+from .basicauthutils import validate_request
+from .response import HttpResponseUnauthorized
+
+
+class BasicAuthMiddleware:
+    def process_request(self, request):
+        validated_username = validate_request(request)
+        if validated_username is None:
+            return HttpResponseUnauthorized()
+        else:
+            return None

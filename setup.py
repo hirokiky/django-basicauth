5b82bb8a948d75faef575d4c65e217252c30521d
rhoboro <rhoboro@gmail.com>
2020-11-16 21:58:52 +0900

    drop unsupported versions

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description="Basic auth utilities for Django.",
     long_description=README + '\n\n' + CHANGES,
     packages=['basicauth'],
-    install_requires=['Django>=1.11'],
+    install_requires=['Django>=2.0'],
     include_package_data=True,
     test_suite="tests",
     zip_safe=False,
